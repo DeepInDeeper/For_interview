9292aaeb00c3f50e2573e2026449a3f63c530f4d
PeterChenYijie <2232941357@qq.com>
2017-05-19 10:01:30 +0800

    a

--- Python/Python Programmer(wechat)/pandas/handle_missing_data.py
+++ Python/Python Programmer(wechat)/pandas/handle_missing_data.py
@@ -2,6 +2,7 @@
 
 from pandas import read_csv
 import numpy
+from sklearn.preprocessing import Imputer
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import KFold
@@ -13,6 +14,8 @@ dataset = read_csv('pima-indians-diabetes.csv',header = None)
 #print (dataset[[1,2,3,4,5]]==0).sum()
 dataset[[1,2,3,4,5]] = dataset[[1,2,3,4,5]].replace(0,numpy.NaN)
 
+
+'''
 dataset.dropna(inplace = True)
 # print (dataset.shape)
 
@@ -24,6 +27,19 @@ model = LinearDiscriminantAnalysis()
 kfold = KFold(n_splits=3, random_state=7)
 result = cross_val_score(model, x, y, cv=kfold, scoring='accuracy')
 print(result.mean())
+'''
+
+'''
+dataset.fillna(dataset.mean(),inplace = True)
+print dataset.isnull().sum()
+'''
+
+
+values = dataset.values
+imputer = Imputer()
+transformed_values = imputer.fit_transform(values)
+print (numpy.isnan(transformed_values).sum())
+
 
 
 
