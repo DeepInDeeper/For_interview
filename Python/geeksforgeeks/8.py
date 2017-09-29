9e1be34b43c41df0767fcdff210d05d921b153e1
peterchenyijie <2232941357@qq.com>
2017-09-29 16:55:22 +0800

    update

--- Python/geeksforgeeks/8.py
+++ Python/geeksforgeeks/8.py
@@ -1,17 +1,23 @@
 #-*-coding:utf-8-*-
-# unsolved
-# remove 是删除首个符合条件的元素。并不是删除特定的索引
+# remark
 # http://practice.geeksforgeeks.org/problems/phone-directory/0
 
-T = int(input())
-while T:
-    N = int(input())
-    str_dic = input().split()
-    query = input()
-    list_out = str_dic
-    for i in range(len(query)):
-        for list_ele in list_out:
-            if str(list_ele[i]) != str(query[i]):
-                list_out.remove(list_ele)
-        print (query[i],list_out)
-    T -= 1
+#code
+def main():
+    for i in range(int(input())):
+        N = int(input())
+        vals = input().split()
+        query = input()
+        for i in range(len(query)):
+            res = set()
+            q = query[:i+1]
+            for v in vals:
+                if v.startswith(q):
+                    res.add(v)
+            if res:
+                print(" ".join(sorted(list(res))))
+            else:
+                print("0")
+                
+if __name__ == "__main__":
+    main()
