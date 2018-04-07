4e2ed2cd3f1c883c64b6cf457fc5d22dc9f2e565
peterchenyijie <2232941357@qq.com>
2018-04-07 17:20:04 +0800

    update

---
+++ Python/Tensorflow/GroupNorm.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+def GroupNorm(x,gamma,beta,G,eps=1e-5):
+    N,C,H,W = x.shape
+    x = tf.reshape(x,[N,G,C//G,H,W])
+    mean,var = tf.nn.moments(x,[2,3,4],keep_dims=True)
+    x = (x - mean)/tf.sqrt(var+eps)
+    x = tf.reshape(x,[N,C,H,W])
+    return x*gamma+beta
+
